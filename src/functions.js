87ae55c1d49aa16b0a97f345552ced6448a9051e
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-10 17:01:26 -0300

    Criando function detecta numero maior

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,16 @@
 // Requisito 1 - Crie a função verifyPalindrome
 
 // Requisito 2 - Crie a função getHighestIndex
-
+function getHighestIndex(array) {
+  let highestValue = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (highestValue > array[index]) {
+      highestValue = index;
+    }
+  }
+    return highestValue;
+}
+console.log(getHighestIndex());
 // Requisito 3 - Crie a função getSmallestIndex
 
 // Requisito 4 - Crie a função getLongestWord
